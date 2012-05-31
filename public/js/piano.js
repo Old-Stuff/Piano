4c3822980c0b0f20ea480052e7a4fa5f7f49bc60
Myles Borins <myles.borins@gmail.com>
2012-05-30 20:36:07 -0400

    Basic Canvas draw of piano, and js event

--- public/js/piano.js
+++ public/js/piano.js
@@ -3,5 +3,30 @@
 var piano_0_1 = piano_0_1 || {};
 
 (function ($, piano) {
-	console.log("hello world");
+	
+    noteOn = function (midinote, id){
+	    console.log(midinote + " " + id);
+	};
+    
+    noteOff = function (midinote, id){
+	    console.log(midinote + " " + id);
+	};
 })(jQuery, piano_0_1);    
+
+// var test = function (string){
+//     console.log(string);
+// }
+
+// function cat(name) {
+//   var that = {};
+//   that.name = name;
+//   that.meow = function () {
+//     console.log(that.name + " says Meow!");
+// 
+//     };
+// 
+//   return that;
+//   }
+// var myCat = cat("Richard");
+// 
+// myCat.meow();
