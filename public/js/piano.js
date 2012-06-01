a0abc577887989462b10801c9c420e726f107e8c
Myles Borins <myles.borins@gmail.com>
2012-06-01 01:26:20 -0400

    Added Note Press Highlight via jquery.svn

--- public/js/piano.js
+++ public/js/piano.js
@@ -1,17 +1,21 @@
 // I have no idea what I'm doing . gif
+// Starting to figure it out though yipee
 
 var myles = myles || {};
-
 
 (function ($) {
     myles.midiToFreq = function(m){
         return Math.pow(2, ((m-69)/12))*440;
     };
-
     myles.piano = function (container){
         var that = {};
         that.container = $(container);
-        
+        that.svg = that.container.svg()
+        that.keys = {
+            white: [60,62,64,65,67,69,71],
+            black: [61,63,66,68,70]
+        };
+        // console.log(that.keys);
         that.noteOn = function(midinote){
             var freq = myles.midiToFreq(midinote);
             that.synth.input("carrier.freq", freq);
@@ -25,9 +29,16 @@ var myles = myles || {};
             that.notes.each(function(i,note){
                 note = $(note);
                 note.mousedown(function(){
+                    note.css('fill', 'yellow');
                     that.noteOn(note[0].id);
                 });
                 note.mouseup(function(){
+                    if($.inArray(parseInt(note[0].id), that.keys.white) != -1){
+                        note.css('fill', 'white');
+                    }
+                    else{
+                        note.css('fill', 'black');
+                    }
                     that.noteOff(note[0].id);
                 });
             });
