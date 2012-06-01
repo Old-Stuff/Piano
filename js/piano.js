6f739e6020a8a4b7e1ce91916ef9a1a0a3a44d9b
Myles Borins <myles.borins@gmail.com>
2012-06-01 02:30:43 -0400

    Dragging Working

--- js/piano.js
+++ js/piano.js
@@ -25,14 +25,33 @@ var myles = myles || {};
             that.synth.input("asr.gate", 0.0);
         };
         that.bindEvents = function(){
+            var lastPressed = {};
+            that.isPressed = false;
             that.notes = that.container.find(".note");
             that.notes.each(function(i,note){
                 note = $(note);
+                note.hover(function(){
+                    if(that.isPressed){
+                        if($.inArray(parseInt(lastPressed[0].id), that.keys.white) != -1){
+                            lastPressed.css('fill', 'white');
+                        }
+                        else{
+                            lastPressed.css('fill', 'black');
+                        }
+                        that.noteOff(lastPressed[0].id);
+                        lastPressed = note;
+                        note.css('fill', 'yellow');
+                        that.noteOn(note[0].id);
+                    }
+                });
                 note.mousedown(function(){
+                    lastPressed = note;
+                    that.isPressed = true;
                     note.css('fill', 'yellow');
                     that.noteOn(note[0].id);
                 });
                 note.mouseup(function(){
+                    that.isPressed = false;
                     if($.inArray(parseInt(note[0].id), that.keys.white) != -1){
                         note.css('fill', 'white');
                     }
