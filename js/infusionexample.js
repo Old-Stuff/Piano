e0d8b1a038f2c2f3d8ca602976b25f94b706a081
Myles Borins <myles.borins@gmail.com>
2012-06-26 11:08:50 -0400

    Oscillator Component Working

--- js/infusionexample.js
+++ js/infusionexample.js
@@ -47,47 +47,110 @@ var tutorials = tutorials || {};
     //         };
     //     };
     
-    // Third is the inclusion of evented components
+    // // Third is the inclusion of evented components
+    //     
+    //     fluid.defaults("tutorials.currencyConverter", {
+    //         gradeNames: ["fluid.modelComponent", "fluid.eventedComponent", "autoInit"],
+    //         model: {
+    //             rates: {
+    //                 euro: 0.712,
+    //                 yen: 81.841,
+    //                 yuan: 6.609,
+    //                 usd: 1.02,
+    //                 rupee: 45.789
+    //             },
+    //             currentSelection: "euro",
+    //             amount: 0,
+    //             convertedAmount: 0
+    //         },
+    //         events: {
+    //             conversionUpdated: null
+    //         },
+    //         finalInitFunction: "tutorials.currencyConverter.finalInit"
+    //     });
+    // 
+    //     tutorials.currencyConverter.finalInit = function (that) {
+    // 
+    //         // Add methods to the component object
+    //         that.updateCurrency = function (newCurrency) {
+    //             that.applier.requestChange("currentSelection", newCurrency);
+    //         };
+    // 
+    //         that.updateRate = function (currency, newRate) {
+    //             that.applier.requestChange("rates." + currency, newRate);
+    //         };
+    // 
+    //         that.convert = function (amount) {
+    //             var convertedAmount = amount * that.model.rates[that.model.currentSelection];
+    //             that.applier.requestChange("convertedAmount", convertedAmount);
+    //             return amount;
+    //         };
+    // 
+    //         that.applier.modelChanged.addListener("convertedAmount", function (model, oldModel, changeRequest) {
+    //             that.events.conversionUpdated.fire(model.convertedAmount);
+    //         });
+    //     };
+    
+    // Using Renderer Component
     
     fluid.defaults("tutorials.currencyConverter", {
-        gradeNames: ["fluid.modelComponent", "fluid.eventedComponent", "autoInit"],
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+        selectors: {
+            amount: ".tut-currencyConverter-amount",
+            currency: ".tut-currencyConverter-currency-selecter",
+            result: ".tut-currencyConverter-result"
+        },
         model: {
             rates: {
-                euro: 0.712,
-                yen: 81.841,
-                yuan: 6.609,
-                usd: 1.02,
-                rupee: 45.789
+                names: ["euro", "yen", "yuan", "usd", "rupee"],
+                values: ["0.712", "81.841", "6.609", "1.02", "45.789"]
             },
-            currentSelection: "euro",
+            currentSelection: "0.712",
             amount: 0,
-            convertedAmount: 0
+            result: 0
         },
         events: {
             conversionUpdated: null
         },
-        finalInitFunction: "tutorials.currencyConverter.finalInit"
+        produceTree: "tutorials.currencyConverter.produceTree",
+        finalInitFunction: "tutorials.currencyConverter.finalInit",
+        renderOnInit: true
     });
+
+    var bindEventHanders = function (that) {
+        // When the model changes, update the resulting "converted" value
+        that.applier.modelChanged.addListener("amount", function () {
+            that.convert(that.model.amount);
+        });
+        that.applier.modelChanged.addListener("currentSelection", function () {
+            that.convert(that.model.amount);
+        });
+        that.applier.modelChanged.addListener("result", function () {
+            that.refreshView();
+        });
+    };
+
+    tutorials.currencyConverter.produceTree = function (that) {
+        return {
+            amount: "${amount}",
+            currency: {
+                optionnames: "${rates.names}",
+                optionlist: "${rates.values}",
+                selection: "${currentSelection}"
+            },
+            result: "${result}"
+        };
+    };
 
     tutorials.currencyConverter.finalInit = function (that) {
 
-        // Add methods to the component object
-        that.updateCurrency = function (newCurrency) {
-            that.applier.requestChange("currentSelection", newCurrency);
-        };
-
-        that.updateRate = function (currency, newRate) {
-            that.applier.requestChange("rates." + currency, newRate);
-        };
-
+        // Add a method to the component object
         that.convert = function (amount) {
-            var convertedAmount = amount * that.model.rates[that.model.currentSelection];
-            that.applier.requestChange("convertedAmount", convertedAmount);
-            return amount;
+            var convertedAmount = parseInt(amount) * that.model.currentSelection;
+            that.applier.requestChange("result", convertedAmount);
+            that.events.conversionUpdated.fire(convertedAmount);
         };
-
-        that.applier.modelChanged.addListener("convertedAmount", function (model, oldModel, changeRequest) {
-            that.events.conversionUpdated.fire(model.convertedAmount);
-        });
+    
+        bindEventHanders(that);
     };
 })(jQuery, fluid_1_4);
---
+++ js/oscillator-infusion.js
@@ -0,0 +1,67 @@
+/*
+Copyright 2011 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+var automm = automm || {};
+
+(function ($, fluid) {
+    fluid.defaults("automm.oscillator", {
+        gradeNames: ["fluid.modelComponent", "autoInit"],
+        postInitFunction: "automm.oscillator.postInitFunction",
+        
+        model: {
+            freq: 440,
+            osc: "flock.ugen.sinOsc",
+            attack: 0.25,
+            sustain: 0.6,
+            release: 0.5,
+            gate: 0
+        }
+    });
+    
+    automm.oscillator.postInitFunction = function (that) {
+        that.osc = flock.synth({
+            id: "carrier",
+            ugen: that.model.osc,
+            freq: that.model.freq,
+            mul: {
+                id: "asr",
+                ugen: "flock.ugen.env.simpleASR",
+                attack: that.model.attack,
+                sustain: that.model.sustain,
+                release: that.model.release
+            }
+        });
+        that.updateFreq = function (newFreq){
+            that.applier.requestChange("freq", newFreq);
+            that.osc.input("carrier.freq", freq);
+        };
+        that.updateOSC = function (newOSC){
+            that.applier.requestChange("osc", newOSC);
+        };
+        that.updateAttack = function (newAttack){
+            that.applier.requestChange("attack", newAttack);
+        };
+        that.updateSustain = function (newSustain){
+            that.applier.requestChange("sustain", newSustain);
+        };
+        that.updateRelease = function (newRelease){
+            that.applier.requestChange("release", newRelease);
+        };
+        that.updateGate = function (newGate){
+            that.applier.requestChange("gate", newGate);
+            that.osc.input("asr.gate", newGate);
+        };  
+        
+        flock.enviro.shared.play();
+        console.log("it's alive");      
+    };
+    
+})(jQuery, fluid_1_4);
