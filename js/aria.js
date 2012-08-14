c80814444f343fa55bab025b63d0ee496744fd14
Myles Borins <myles.borins@gmail.com>
2012-08-14 13:35:58 -0400

    Selecting in the right order

--- js/aria.js
+++ js/aria.js
@@ -37,10 +37,10 @@ var automm = automm || {};
     automm.aria.preInitFunction = function (that) {
         that.getNotes = function () {
             // Dump all notes within the container into an array
-            that.model.renderedNotes = that.container.find(".note");
+            var domNotes = that.container.find(".note");
 
             // Iterate through the array and replace the notes with their id's a.k.a note numbers
-            fluid.each(that.model.renderedNotes, function (note, i) {
+            fluid.each(domNotes, function (note, i) {
                 that.model.renderedNotes[i] = note.id;
             });
 
@@ -49,42 +49,51 @@ var automm = automm || {};
 
             // Iterate again, this time replace the id numbers with note names
             fluid.each(that.model.renderedNotes, function (note, i) {
-                that.model.renderedNotes[i] = [note, that.model.notes[note % 12] + (Math.floor(note / 12) - 1)];
+                that.model.renderedNotes[i] = {"number": note, "name": (that.model.notes[note % 12] + (Math.floor(note / 12) - 1))};
             });
 
         };
 
         that.setTitle = function () {
-            var ariaContainer = $("#aria");
+            var ariaContainer = that.container.find("#aria");
             ariaContainer.append("<div id='ariaTitle'>The Automagic Music Maker</div>");
         };
 
         that.render = function () {
-            var ariaContainer = $("#aria");
+            var ariaContainer = that.container.find("#aria");
             if (ariaContainer.length < 1) {
                 that.container.append("<div id='aria' style='display:none;'><ul></ul></div>");
-                ariaContainer = $("#aria").children();
+                ariaContainer = that.container.find("#aria").children();
             } else {
                 ariaContainer.empty();
                 ariaContainer.append('<ul></ul>');
             }
             fluid.each(that.model.renderedNotes, function (note) {
-                ariaContainer.append("<li id='aria" + note[0] + "'>" + note[1] + "</li>");
+                ariaContainer.append("<li id='aria" + note.number + "'>" + note.name + "</li>");
             });
             that.setTitle();
         };
 
+        // that.noteArray = function () {
+        //     fluid.each(that.model.renderedNotes, function (note, i) {
+        //         that.model.noteArray[i] = that.container.find("#" + note.number);
+        //     });
+        //     that.model.noteArray = $(that.model.noteArray);
+        // };
+
         that.fluidInit = function () {
-            var instrumentType = $(that.container.children()[0]);
+            var instrumentType = that.container.children().eq(0),
+                noteArray = $(automm.fetchNotes(that.container, that.model.renderedNotes));
             instrumentType.fluid("tabbable");
             instrumentType.fluid("selectable", {
                 // the default orientation is vertical, so we need to specify that this is horizontal.
                 // this affects what arrow keys will move selection
-                direction: fluid.a11y.orientation.HORIZONTAL
-                // 
-                // onSelect: function (thumbEl) {
-                //     $(thumbEl).addClass(demo.imageViewer.styles.selected);
-                // },
+                direction: fluid.a11y.orientation.HORIZONTAL,
+                selectableElements: noteArray,
+
+                onSelect: function (note) {
+                    console.log(note);
+                }
                 // onUnselect: function (thumbEl) {
                 //     $(thumbEl).removeClass(demo.imageViewer.styles.selected);
                 // }
@@ -94,13 +103,20 @@ var automm = automm || {};
         that.update = function () {
             that.getNotes();
             that.render();
+            that.fluidInit();
         };
     };
 
     automm.aria.postInitFunction = function (that) {
-        that.fluidInit();
         that.update();
         that.events.afterUpdate.addListener(that.update);
+    };
+
+    automm.fetchNotes = function (container, noteModel) {
+        return fluid.transform(noteModel, function (note) {
+            var noteSelector = "#" + note.number;
+            return container.find(noteSelector)[0];
+        });
     };
 
 }(jQuery));
