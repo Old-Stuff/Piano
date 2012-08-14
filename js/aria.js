c36a86ce05b0ee0f266257a7b1dd29c0ea4506bf
Myles Borins <myles.borins@gmail.com>
2012-08-14 17:01:37 -0400

    broke out handler

--- js/aria.js
+++ js/aria.js
@@ -59,42 +59,54 @@ var automm = automm || {};
 
         that.setTitle = function () {
             var ariaContainer = that.container.find("#aria");
+            // Append a div that will be used to title the aria application
             ariaContainer.append("<div id='ariaTitle'>The Automagic Music Maker</div>");
         };
 
         that.render = function () {
+            // Find jquery for aria element
             var ariaContainer = that.container.find("#aria");
+            // If that container does not exist, make it
             if (ariaContainer.length < 1) {
                 that.container.append("<div id='aria' style='display:none;'><ul></ul></div>");
                 ariaContainer = that.container.find("#aria").children();
             } else {
+                // If it does exist empty it out and put an unordered list in it
                 ariaContainer.empty();
                 ariaContainer.append('<ul></ul>');
             }
+            // For each note that has been rendered to the screen make a list element
             fluid.each(that.model.renderedNotes, function (note) {
                 ariaContainer.append("<li id='aria" + note.number + "'>" + note.name + "</li>");
             });
+            // Call the function to make the div used to title application
             that.setTitle();
         };
 
-        that.fluidInit = function () {
-            var instrumentType = that.container.children().eq(0),
-                noteArray = $(automm.fetchNotes(that.container, that.model.renderedNotes)),
-                handler = function (evt) {
-                    var note = evt.target,
-                        noteId = note.id,
-                        noteState = $.inArray(noteId, that.model.playingNotes);
-                    note = $(note);
-                    if (noteState > -1) {
-                        that.model.playingNotes.splice(noteState, 1);
-                        that.events.afterNote.fire(note);
-                    } else {
-                        that.model.playingNotes[that.model.playingNotes.length] = noteId;
-                        that.events.onNote.fire(note);
-                    }
-                };
+        that.onActivation = function (note) {
+            // get the id of the current note and see if it is already playing
+            var noteId = note.id,
+                noteState = $.inArray(noteId, that.model.playingNotes);
+            note = $(note);
+            // If the note is in the playingNotes array, splice it out and fire afterNote
+            if (noteState > -1) {
+                that.model.playingNotes.splice(noteState, 1);
+                that.events.afterNote.fire(note);
+            // If it is not in the array, put it in there and fire onNote
+            } else {
+                that.model.playingNotes[that.model.playingNotes.length] = noteId;
+                that.events.onNote.fire(note);
+            }
+        };
 
+        that.fluidInit = function () {
+            // Find type of instrument that has been rendered
+            var instrumentType = that.container.children().eq(0),
+                // Create an array fille dwith objects stating note numbers and names of all rendered notes
+                noteArray = $(automm.fetchNotes(that.container, that.model.renderedNotes));
+            // Make the container tabbable
             instrumentType.fluid("tabbable");
+            // Make the elements inside selectable
             instrumentType.fluid("selectable", {
                 // the default orientation is vertical, so we need to specify that this is horizontal.
                 // this affects what arrow keys will move selection
@@ -105,11 +117,11 @@ var automm = automm || {};
                 onSelect: function (note) {
                     note.focus();
                 }
-                // onUnselect: function (thumbEl) {
-                //     $(thumbEl).removeClass(demo.imageViewer.styles.selected);
-                // }
             });
-            instrumentType.fluid("activatable", handler);
+            // Set the handler to be used when notes are activated
+            instrumentType.fluid("activatable", function (evt) {
+                that.onActivation(evt.target);
+            });
         };
 
         that.update = function () {
@@ -125,6 +137,7 @@ var automm = automm || {};
     };
 
     automm.fetchNotes = function (container, noteModel) {
+        // take a container and model of renderedNotes, return an array of the elements of those notes
         return fluid.transform(noteModel, function (note) {
             var noteSelector = "#" + note.number;
             return container.find(noteSelector)[0];
