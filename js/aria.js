3024a726b27ea2766cc9d24d6f905b8bce60c753
Myles Borins <myles.borins@gmail.com>
2012-08-20 12:53:28 -0400

    added aria description

--- js/aria.js
+++ js/aria.js
@@ -11,7 +11,7 @@ licenses are at the root of the Piano directory.
 
 */
 
-/*global jQuery, fluid, document, console*/
+/*global jQuery, fluid, document*/
 
 var automm = automm || {};
 
@@ -62,9 +62,30 @@ var automm = automm || {};
 
         that.setTitle = function () {
             var ariaContainer = that.container.find("#aria"),
-                instrumentType = that.container.children()[0];
+                instrumentType = that.container.children()[0].id;
             // Append a div that will be used to title the aria application
-            ariaContainer.append("<div id='ariaTitle'>AutoMM " + that.container[0].id + " type: " + instrumentType.id + "</div>");
+            ariaContainer.append("<div id='ariaTitle'>AutoMM " + that.container[0].id + " type: " + instrumentType + "</div>");
+        };
+
+        that.setDescription = function () {
+            var ariaContainer = that.container.find("#aria"),
+                instrumentType = that.container.children()[0].id;
+            ariaContainer.append("<div id='ariaDescription'>Welcome to the Automagic Music Maker "
+                + "Within this application you will find a " + instrumentType + " in which you can interact with.  "
+                + "The " + instrumentType + " has " + that.model.renderedNotes.length + " notes "
+                + "starting with " + that.model.renderedNotes[0].name + " and ending with "
+                + that.model.renderedNotes[that.model.renderedNotes.length - 1].name
+                + " you can use the left and right keys to move between notes, and press the space bar to activate them. "
+                + "You can deactivate individual notes by pressing spacebar a second time while it is selected. "
+                + "If you find that you have too many notes playing at a time, you can stop them all by hitting escape. "
+                + "The application also has an arpeggiator, which plays note patterns based on the first note you press. "
+                + "To enable the arpeggiator, press alt at any time. "
+                + "This application does not follow aria standards, and you will not hear any notification of where your "
+                + "current selection is, but don't fret, this was a design choice. It was found to be too distracting to "
+                + "play the piano while having your screen reader constantly yelling over top. "
+                + "I plan to add keyboard bindings in the near future, this should make the application much more pleasurable "
+                + "to interact with.  If you have any question or suggestions please email me at myles DOT borins AT gmail DOT"
+                + " com or tweet me @the_alpha_nerd" + "<div>");
         };
 
         that.render = function () {
@@ -72,10 +93,11 @@ var automm = automm || {};
             var ariaContainer = that.container.find("#aria");
             // If that container does not exist, make it
             if (ariaContainer.length < 1) {
-                that.container.append("<div id='aria' style='display:none;'><ul></ul></div>");
+                that.container.append("<div id='aria' style='display:none;'></div>");
             }
             // Call the function to make the div used to title application
             that.setTitle();
+            that.setDescription();
         };
 
         // The Below function is called when spacebar is hit on a selected Note
