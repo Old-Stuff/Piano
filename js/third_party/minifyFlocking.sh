5b85c06f0e682eb51a653991aeb871c3f2cdb4f1
Colin Clark <colinbdclark@gmail.com>
2013-05-18 12:28:43 -0600

    Updates builds to latest Flocking and Infusion.

--- js/third_party/minifyFlocking.sh
+++ js/third_party/minifyFlocking.sh
@@ -5,5 +5,15 @@ if [ -e "./flocking.min.js" ]; then
 	rm ./flocking.min.js
 fi
 cat ./Flocking/flocking/flocking-core.js | jsmin > flocking.min.js
+cat ./Flocking/flocking/flocking-scheduler.js| jsmin >> flocking.min.js
+cat ./Flocking/flocking/flocking-firefox.js| jsmin >> flocking.min.js
+cat ./Flocking/flocking/flocking-webaudio.js| jsmin >> flocking.min.js
 cat ./Flocking/flocking/flocking-parser.js| jsmin >> flocking.min.js
 cat ./Flocking/flocking/flocking-ugens.js | jsmin >> flocking.min.js
+
+# check if infusion-framework.min.js exists yet
+if [ -e "./infusion-framework.min.js" ]; then
+	rm ./infusion-framework.min.js
+fi
+
+cat ./infusion-framework.js | jsmin > infusion-framework.min.js
